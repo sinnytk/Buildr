02c9b9115fcdae241de1a0114dd6f833e2b3fc77
Tarun Kumar <ttarunkkumar99@gmail.com>
2019-04-04 01:45:15 +0500

    Added support for Galaxy.pk - CPUs done. TODO: Motherboards

--- testscrap.py
+++ testscrap.py
@@ -38,15 +38,37 @@ def PAKDUKAAN(type,link):
 			pp=product.find("span",class_="price").text.strip()
 			products.append(CPU(pn,pp))
 	return products
+def GALAXY(type, link):
+	products = []
+	req = requests.get(link)
+	soup = BeautifulSoup(req.content, 'html.parser')
+	pages = len(soup.find("ul",class_="pages-items").find_all("li")[2])-1
+	for page in range(pages):
+		pagelink = link+"?p="+str(page+1)
+		req=requests.get(pagelink)
+		soup = BeautifulSoup(req.content,"html.parser")
+		productlist = soup.find_all("li",class_="product-item")
+		productlist = productlist[1:]
+		for product in productlist:
+			pn=product.find("a",class_="product-item-link").text.strip()
+			pp=product.find("span",class_="price").text
+			pp=pp[:-3]
+			products.append(CPU(pn,pp))
+	return products
+		
 
 def CPUscrap():
-	links = ['http://czone.com.pk/processors-pakistan-ppt.85.aspx','https://www.pakdukaan.com/pc-hardware-accessories/processors']
+	links = ['http://czone.com.pk/processors-pakistan-ppt.85.aspx','https://www.pakdukaan.com/pc-hardware-accessories/processors','https://www.galaxy.pk/pc-addons/processor/intel.html']
 	CPUs = []
 	CPUs.extend(CZONE('CPU',links[0]))
 	CPUs.extend(PAKDUKAAN('CPU',links[1]))
+	CPUs.extend(GALAXY('CPU',links[2]))
 	return CPUs
 
-
+class MOBO:
+	def __init__(self,name, price):
+		data = self.normalize(name, price)
+		
 class CPU:
 	def __init__(self,name, price):
 		data = self.normalize(name, price)
@@ -62,10 +84,10 @@ class CPU:
 	def normalize(self,name, price):
 		data = []
 		name.replace(',','').replace('Processor','').replace('Desktop',''); #removing unrequired tags
-		data.append(re.search(("[0-9]{4}(K|T|P|U)?"),name).group(0)) #getting the CPU by using regex to find 4 consecutive occurences of numbers eg 8000k
+		data.append(re.search(("[0-9]{4}(K|k|T|t|P|p|U|u|X|x)?"),name).group(0).upper()) #getting the CPU by using regex to find 4 consecutive occurences of numbers eg 8000k
 		data.append(name) #complete title of the product for descriptive purposes
 		data.append(re.search(("(Intel|AMD)"),name).group(0)) #regex to find product brand, amd or intel
-		data.append(re.search(("(Core|Ryzen)( |-)(i[0-9]|[0-9])"),name).group(0)) #regex to find series e.g Ryzen 5 or Core i7
+		data.append(re.search(("(Core|Ryzen)( |-)((i|I)[0-9]|[0-9])"),name).group(0)) #regex to find series e.g Ryzen 5 or Core i7
 		if(data[2][0]=='I'): #finding the generation of a cpu from it's ID, intel's first letter of code defines the generation, eg 8000k means 8th gen
 			data.append(data[0][0])
 		else:
@@ -80,35 +102,6 @@ class CPU:
 
 	def printDetails(self):
 		print("CPU ID: %s\nCPUtitle: %s\nCPUbrand: %s\nCPUseries: %s\nCPUgen: %s\nUnlocked: %s\nPrice: %d" %(self.CPUid, self.CPUtitle, self.CPUbrand, self.CPUseries, self.CPUgen, self.CPUunlocked, self.CPUprice))
-
-
-# def scrap(link):	
-# 	productnames = []
-# 	productprices = []
-# 	req = requests.get(link)
-# 	soup = BeautifulSoup(req.content, "html.parser")
-# 	if(soup.find(id="anLastPageBottom").has_attr('href')):
-# 		pages = int(soup.find(id="anLastPageBottom")['href'][-1])
-# 	else:
-# 		print("1 page only")
-# 		pages = 1
-# 	print("product \t price")
-# 	for page in range(pages):
-# 		pagelink = link+"?page="+str(page+1)
-# 		req = requests.get(pagelink)
-# 		soup = BeautifulSoup(req.content, "html.parser")
-# 		for product in range(len(soup.find_all("div",class_="template"))-2):
-# 			pn='rptListView_ctl0'+str(product)+'_anProductName'
-# 			pp='rptListView_ctl0'+str(product)+'_spnPrice'
-# 			productnames.append(soup.find(id=pn).text.strip())
-# 			productprices.append(soup.find(id=pp).text.strip())
-
-# 	return productnames, productprices
-def writecsv(name, prodnames, prodprices):
-	productfile = open(name+"_prices.csv",mode="w")
-	productfile = csv.writer(productfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-	for pn,pp in zip(prodnames,prodprices):
-		productfile.writerow([pn.split(",")[0],pp])
 	
 def main():
 	# cpuname,cpuprices = scrap("http://czone.com.pk/processors-pakistan-ppt.85.aspx")
@@ -117,7 +110,7 @@ def main():
 	CPUs = CPUscrap()
 	print(len(CPUs))
 	for CPU in CPUs:
-		print(CPU.printDetails())
+		CPU.printDetails()
 		print("\n\n")
 	# writecsv("CPU",cpuname,cpuprices)
 	# writecsv("MOBO",moboname,moboprices)
