b4883a5df395afe21c423fc3dc50590422b3cc03
Tarun Kumar <sinnytk@gmail.com>
2019-04-19 03:55:38 +0500

    UPDATE: Better Galaxy scrape. MySQL connector embedded

--- datascraper.py
+++ datascraper.py
@@ -1,6 +1,8 @@
 import requests
 import re
+import config #contains the details for the database connection
 import csv
+import mysql.connector
 from bs4 import BeautifulSoup
 
 #creating a class for easy usage of objects
@@ -10,24 +12,24 @@ def CZONE(type,link):
 	soup = BeautifulSoup(req.content,'html.parser')
 	if(soup.find(id="anLastPageBottom").has_attr('href')):
 		pages = int(soup.find(id="anLastPageBottom")['href'][-1])
-		print(pages)
 	else:
 		pages = 1
 	for page in range(1,pages+1):
 		pagelink = link+"?page="+str(page)
-		print(pagelink)
 		req = requests.get(pagelink)
+		print("Scraping CZONE for "+type+" on page: "+str(page))
 		soup = BeautifulSoup(req.content, "html.parser")
 		for product in range(len(soup.find_all("div",class_="product"))):
 			pn='rptListView_ctl0'+str(product)+'_anProductName'
+			pl="https://www.czone.com.pk"+soup.find(id=pn)['href']
 			pn = soup.find(id=pn).text.strip()
 			pp='rptListView_ctl0'+str(product)+'_spnPrice'
 			pp = soup.find(id=pp).text.strip()
 			if(type=='CPU'):
-				products.append(CPU(pn,pp))
+				products.append(CPU(pn,pp,pl))
 			elif(type=='MOBO'):
 				code=soup.find(id='rptListView_ctl0'+str(product)+'_spnProductCode').text.strip()
-				products.append(MOBO(pn,pp,code))
+				products.append(MOBO(pn,pp,code,pl))
 
 	return products
 def PAKDUKAAN(type,link):
@@ -35,31 +37,37 @@ def PAKDUKAAN(type,link):
 	req = requests.get(link)
 	soup = BeautifulSoup(req.content, 'html.parser')
 	pages = len(soup.find_all('ol')[0].find_all('li'))-1
-	for page in range(pages):
-		pagelink = link+'?p='+str(page+1)
+	for page in range(1,pages+1):
+		pagelink = link+'?p='+str(page)
 		req = requests.get(pagelink)
+		print("Scraping PAKDUKAAN for "+type+" on page: "+str(page))
 		soup = BeautifulSoup(req.content, "html.parser")
 		for product in soup.find_all("div",class_="product-item-info"):
-			pn=product.find("h2",class_="product-name").a['title'].strip().replace(u"\u2122", '').replace(u"\u00AE",'')
+			p=product.find("h2",class_="product-name")
+			pl=p.a['href']
+			pn=p.a['title'].strip().replace(u"\u2122", '').replace(u"\u00AE",'')
 			pp=product.find("span",class_="price").text.strip()
-			products.append(CPU(pn,pp))
+			products.append(CPU(pn,pp,pl))
 	return products
 def GALAXY(type, link):
 	products = []
 	req = requests.get(link)
 	soup = BeautifulSoup(req.content, 'html.parser')
 	pages = len(soup.find("ul",class_="pages-items").find_all("li")[2])-1
-	for page in range(pages):
-		pagelink = link+"?p="+str(page+1)
+	for page in range(1,pages+1):
+		pagelink = link+"&p="+str(page)
 		req=requests.get(pagelink)
+		print("Scraping GALAXY for "+type+" on page: "+str(page))
 		soup = BeautifulSoup(req.content,"html.parser")
 		productlist = soup.find_all("li",class_="product-item")
-		productlist = productlist[1:]
+		productlist=productlist[1:]
 		for product in productlist:
-			pn=product.find("a",class_="product-item-link").text.strip()
+			p=product.find("a",class_="product-item-link")
+			pl=p['href']
+			pn=p.text.strip()
 			pp=product.find("span",class_="price").text
 			pp=pp[:-3]
-			products.append(CPU(pn,pp))
+			products.append(CPU(pn,pp,pl))
 	return products
 def SHINGPOINT(type, link):
 	products = []
@@ -72,34 +80,31 @@ def SHINGPOINT(type, link):
 	for page in range(1,pages+1):
 		pagelink = link+"?page="+str(page)
 		req = requests.get(pagelink)
+		print("Scraping SHINGPOINT for "+type+" on page: "+str(page))
 		soup = BeautifulSoup(req.content, "html.parser")
 		for product in range(len(soup.find_all("div",class_="product"))):
 			pn='rptListView_ctl0'+str(product)+'_anProductName'
-			
-			tempsoup = BeautifulSoup(requests.get("https://www.shingpoint.com.pk"+((soup.find(id=pn))['href'])).content,'html.parser')
+			pl="https://www.shingpoint.com.pk"+(soup.find(id=pn))['href']
+			tempsoup = BeautifulSoup((requests.get(pl)).content,'html.parser')
 			pn = soup.find(id=pn).text.strip()
 			pp='rptListView_ctl0'+str(product)+'_spnPrice'
 			pp = soup.find(id=pp).text.strip()
-			
 			code = tempsoup.find(id='spnProductCode').text.strip()
-			print(str(product)+'\t'+pn[:10]+'\t'+pp+'\t'+code)
-			products.append(MOBO(pn,pp,code))
-			
-
+			products.append(MOBO(pn,pp,code,pl))
 	return products
 def MOBOscrap():
 	links=['http://czone.com.pk/motherboards-pakistan-ppt.157.aspx','https://www.shingpoint.com.pk/motherboards-pakistan-ppt.10302.aspx']
-	CPUS = set()
+	MOBOs = set()
 	czone_prices=CZONE('MOBO',links[0])
 	shingpoint_price=SHINGPOINT('MOBO',links[1])
 	for product in czone_prices:
-		CPUS.add(product)
+		MOBOs.add(product)
 	for product in shingpoint_price:
-		CPUS.add(product)
-	return CPUS, czone_prices, shingpoint_price
+		MOBOs.add(product)
+	return MOBOs, czone_prices, shingpoint_price
 
 def CPUscrap():
-	links = ['http://czone.com.pk/processors-pakistan-ppt.85.aspx','https://www.pakdukaan.com/pc-hardware-accessories/processors','https://www.galaxy.pk/pc-addons/processor/intel.html']
+	links = ['http://czone.com.pk/processors-pakistan-ppt.85.aspx','https://www.pakdukaan.com/pc-hardware-accessories/processors','https://www.galaxy.pk/pc-addons.html?cat=1276']
 	CPUs = set()
 	czone_prices=CZONE('CPU',links[0])
 	pakdukaan_prices=PAKDUKAAN('CPU',links[1])
@@ -114,18 +119,19 @@ def CPUscrap():
 	return CPUs, czone_prices, pakdukaan_prices, galaxy_prices
 
 class MOBO:
-	def __init__(self,name, price,code):
+	def __init__(self,name, price,code,link):
 		data = self.normalize(name, price, code)
-		self.id = data['id']
-		self.title = data['title']
-		self.chipset=data['chipset']
-		self.vendor=data['vendor']
+		self.id = data['id'].upper()
+		self.title = data['title'].upper()
+		self.chipset=data['chipset'].upper()
+		self.link=link
+		self.vendor=data['vendor'].upper()
 		self.brand,self.socket=self.getSocket()
 		self.price=data['price']
-		def __eq__(self, other):
-			return self.CPUid == other.CPUid
-		def __hash__(self):
-			return hash (self.CPUid)	
+	def __eq__(self, other):
+			return self.id == other.id
+	def __hash__(self):
+			return hash(self.id)
 	def getSocket(self):
 		if(self.chipset not in ['X470','X370','B450','B350','A320','X300','A300','B300']):
 			if(re.search('[HBQPZ]5[57]$',self.chipset)):
@@ -141,13 +147,13 @@ class MOBO:
 			elif(re.search('[HBQZ][3][0-9]0$',self.chipset)):
 				socket='LGA 1151-2'
 			else:
-				socket='Invalid'
-			return 'Intel',socket
+				socket='NA'
+			return 'INTEL',socket
 		else:
 			return 'AMD','AM4'
 	def normalize(self,name,price,code):
 		data = {}
-		data['id']=code.replace(' ','-').replace('(','-').replace(')','-').upper()
+		data['id']=code.replace('-','').replace(' ','-').replace('(','-').replace(')','-').upper()
 		data['title']=name
 		data['vendor']=name.split(' ',1)[0]#retrieving the vendor name, which is always at front
 		data['chipset']=(re.search(('[AHBQZX]([0-9])+'),name).group(0)) if (re.search(('[AHBQZX]([0-9])+'),name)) else 'not available'
@@ -156,69 +162,70 @@ class MOBO:
 		
 
 class CPU:
-	def __init__(self,name, price):
+	def __init__(self,name, price,link):
 		data = self.normalize(name, price)
-		self.CPUid = data['id']
-		self.CPUtitle = data['title']
-		self.CPUbrand = data['brand']
-		self.CPUseries = data['series']
-		self.CPUgen = data['gen']
-		self.CPUunlocked = data['unlocked']
-		self.CPUprice = data['price']
-		self.CPUsocket, self.CPUcodename = self.getSocket()
+		self.id = data['id'].upper()
+		self.title = data['title'].upper()
+		self.brand = data['brand'].upper()
+		self.series = data['series'].upper()
+		self.gen = data['gen'].upper()
+		self.unlocked = data['unlocked'].upper()
+		self.price = data['price']
+		self.link=link
+		self.socket, self.codename = self.getSocket()
 	def __eq__(self, other):
-		return self.CPUid == other.CPUid
+		return self.id == other.id
 	def __hash__(self):
-		return hash (self.CPUid)
+		return hash(self.id)
 	
 	def getSocket(self):
-		socket = 'None'
-		codename = 'None'
-		if(self.CPUbrand=='Intel'):
-			if(re.search(("2[0-7]{4}"),self.CPUid)):
+		socket = 'NA'
+		codename = 'NA'
+		if(self.brand=='INTEL'):
+			if(re.search(("2[0-7]{4}"),self.id)):
 				socket = 'LGA 1155'
 				codename = 'Sandy Bridge'
-			elif(re.search(("3[0-7]{4}"),self.CPUid)):
+			elif(re.search(("3[0-7]{4}"),self.id)):
 				socket = 'LGA 1155'
 				codename = 'Ivy Bridge'
-			elif(re.search(("4[0-7]{4}"),self.CPUid)):
+			elif(re.search(("4[0-7]{4}"),self.id)):
 				socket = 'LGA 1150'
 				codename = 'Haswell'
-			elif(re.search(("5[0-7]{4}"),self.CPUid)):
+			elif(re.search(("5[0-7]{4}"),self.id)):
 				socket = 'LGA 1150'
 				codename = 'Broadwell'
-			elif(re.search(("6[0-7]{4}"),self.CPUid)):
+			elif(re.search(("6[0-7]{4}"),self.id)):
 				socket = 'LGA 1151'
 				codename = 'Skylake'
-			elif(re.search(("7[0-7]{4}"),self.CPUid)):
+			elif(re.search(("7[0-7]{4}"),self.id)):
 				socket = 'LGA 1151'
 				codename = 'Kabylake'
-			elif(re.search(("8[0-9]{4}"),self.CPUid)):
+			elif(re.search(("8[0-9]{4}"),self.id)):
 				socket = 'LGA 1151-2'
 				codename = 'Coffeelake'
-			elif(re.search(("9[0-9]{4}"),self.CPUid)):
+			elif(re.search(("9[0-9]{4}"),self.id)):
 				socket = 'LGA 1151-2'
 				codename = 'Coffeelake'
-			elif(re.search(("3[8-9]{4}"),self.CPUid)):
+			elif(re.search(("3[8-9]{4}"),self.id)):
 				socket = 'LGA 2011'
 				codename = 'Sandy Bridge-E'
-			elif(re.search(("4[8-9]{4}"),self.CPUid)):
+			elif(re.search(("4[8-9]{4}"),self.id)):
 				socket = 'LGA 2011'
 				codename = 'Ivy Bridge-E'
-			elif(re.search(("5[8-9]{4}"),self.CPUid)):
+			elif(re.search(("5[8-9]{4}"),self.id)):
 				socket = 'LGA 2011'
 				codename = 'Haswell-E'
-			elif(re.search(("6[8-9]{4}"),self.CPUid)):
+			elif(re.search(("6[8-9]{4}"),self.id)):
 				socket = 'LGA 2011'
 				codename = 'Broadwell-E'
 		else:
-			if(re.search(("1[2-9]{3}"),self.CPUid)):
+			if(re.search(("1[2-9]{3}"),self.id)):
 				socket = 'AM4'
 				codename = 'Summit Ridge'
-			elif(re.search(("2[2-9]{3}"),self.CPUid)):
+			elif(re.search(("2[2-9]{3}"),self.id)):
 				socket = 'AM4'
 				codename = 'Pinnacle Ridge'
-		return socket,codename
+		return socket.upper(),codename.upper()
 
 	#function to normalize/parse the content of a CPU product title
 	def normalize(self,name, price):
@@ -240,42 +247,46 @@ class CPU:
 
 	
 def main():
-	#available_cpus, czone_cpus, pakdukaan_cpus, galaxy_cpus = CPUscrap()
-	available_mobos, czone_mobos,shingpoint_mobos = MOBOscrap()
-	print('id\tbrand\tvendor\tchipset\tsocket')
-	for mobos in czone_mobos:
-		print('%-30s %-10s %-10s %-10s %-10s'% (mobos.id, mobos.brand, mobos.vendor, mobos.chipset, mobos.socket))
-	print("\n\nSHINGPOINT")
-	for mobos in shingpoint_mobos:
-		print('%-30s %-10s %-10s %-10s %-10s'% (mobos.id, mobos.brand, mobos.vendor, mobos.chipset, mobos.socket))
-	with open('shingpoint_mobos.csv',mode='w') as csv_file:
-		writer = csv.writer(csv_file,delimiter=',')
-		for mobo in shingpoint_mobos:
-			writer.writerow([mobo.id, mobo.brand, mobo.vendor, mobo.chipset, mobo.socket,mobo.price])
-	with open('czone_mobos.csv',mode='w') as csv_file:
-		writer = csv.writer(csv_file,delimiter=',')
-		for mobo in czone_mobos:
-			writer.writerow([mobo.id, mobo.brand, mobo.vendor, mobo.chipset, mobo.socket,mobo.price])
+	dbconn=mysql.connector.connect(
+		host=config.host,
+		user=config.user,
+		passwd=config.passwd,
+		database=config.database
+	)
 
-	#for avail in available_cpus:
-	#	print(avail.CPUid)
-	
-	"""with open('available_cpus.csv', mode='w') as csv_file:
-		writer = csv.writer(csv_file, delimiter=',')
-		for CPU in available:
-			writer.writerow([CPU.CPUid,CPU.CPUbrand,CPU.CPUtitle,CPU.CPUseries,CPU.CPUgen,CPU.CPUcodename,CPU.CPUsocket,CPU.CPUunlocked])
-	with open('czone_cpus.csv', mode='w') as csv_file:
-		writer = csv.writer(csv_file, delimiter=',')
-		for CPU in czone_cpus:
-			writer.writerow([CPU.CPUid,CPU.CPUprice])
-	with open('pakdukaan_cpus.csv', mode='w') as csv_file:
-		writer = csv.writer(csv_file, delimiter=',')
-		for CPU in pakdukaan_cpus:
-			writer.writerow([CPU.CPUid,CPU.CPUprice])
-	with open('galaxy_cpus.csv', mode='w') as csv_file:
-		writer = csv.writer(csv_file, delimiter=',')
-		for CPU in galaxy_cpus:
-			writer.writerow([CPU.CPUid,CPU.CPUprice])"""
+	available_cpus, czone_cpus, pakdukaan_cpus, galaxy_cpus = CPUscrap()
+	available_mobos, czone_mobos,shingpoint_mobos = MOBOscrap()
+	try:
+		dbconn=mysql.connector.connect(
+		host=config.host,
+		user=config.user,
+		passwd=config.passwd,
+		database=config.database
+		)
+		#id, brand, desc, series, gen, socket, codename, unlocked for CPU
+		#id,brand,title,chipset,vendor,socket for MOBO
+		CPUdata = []
+		MOBOdata = []
+		for cpu in available_cpus:
+			
+			CPUdata.append(tuple((cpu.id,cpu.brand,cpu.title,cpu.series,cpu.gen,cpu.socket,cpu.codename,cpu.unlocked)))
+		for mobo in available_mobos:
+			MOBOdata.append(tuple((mobo.id,mobo.brand,mobo.title,mobo.chipset,mobo.vendor,mobo.socket)))
+		
+		CPUquery="INSERT INTO processor values(%s, %s, %s, %s, %s, %s, %s, %s)"
+		MOBOquery="INSERT INTO motherboard values(%s, %s, %s, %s, %s, %s)"
+		cursor = dbconn.cursor(prepared=True)
+		result = cursor.executemany(CPUquery,CPUdata)
+		#print(cursor.rowcount() + " records inserted into processor")
+		result = cursor.executemany(MOBOquery,MOBOdata)
+		dbconn.commit()
+		#print(cursor.rowcount()+ "records inserted into motherboard")
+	except mysql.connector.Error as error:
+		print("Fail {}".format(error))
+	finally:
+		if(dbconn.is_connected()):
+			cursor.close()
+			dbconn.close()
 
 if __name__ == "__main__":
 	main()
