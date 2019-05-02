143a32109b7ad31c18955ea4572d49f40c4785a7
Tarun Kumar <sinnytk@gmail.com>
2019-05-02 18:28:59 +0500

    Upgrade detail.html and product_detail view

--- Buildr/product/views.py
+++ Buildr/product/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, RamPrices, ProcessorPrices, MotherboardPrices, GpuPrices
 
 def product_detail_view(request, *args, **kwargs):
     obj = Product.objects.get(id=kwargs['id'])
+    if(obj.category=="RAM"):
+        prices=RamPrices.objects.all().filter(id=obj.id)
+    elif(obj.category=="CPU"):
+        prices=ProcessorPrices.objects.all().filter(id=obj.id)
+    elif(obj.category=="MOBO"):
+        prices=MotherboardPrices.objects.all().filter(id=obj.id)
+    elif(obj.category=="GPU"):
+        prices=GpuPrices.objects.all().filter(id=obj.id)
     context = {
-        'object':obj
+        'object':obj,
+        'prices':prices
     }
 
     return render(request,'product/detail.html',context)
