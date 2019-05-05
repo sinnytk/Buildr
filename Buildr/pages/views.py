4a76622dcfb0ba1400f19237ef6cd2ec33fdf48f
Tarun Kumar <sinnytk@gmail.com>
2019-05-05 06:12:40 +0500

    Add sort by option with POST on change

--- Buildr/pages/views.py
+++ Buildr/pages/views.py
@@ -2,29 +2,114 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from product.models import Product
 def home_view(request, *args, **kwargs):
+    products=Product.objects.all()
+    sortvalue=0
+    if request.method == 'POST':
+        case=int(request.POST.get('sort'))
+        if(case == 1):
+            sortvalue=1
+            products = Product.objects.all().order_by("min_price")
+        elif(case == 2):
+            sortvalue=2
+            products = Product.objects.all().order_by("-min_price")
+        elif(case == 3):
+            sortvalue=3
+            products = Product.objects.all().order_by("title")
+        elif(case == 4):
+            sortvalue=4
+            products = Product.objects.all().order_by("-title")
     context = {
-        'all_products':Product.objects.all()
+        'sortvalue':sortvalue,
+        'all_products': products
     }
     return render(request,"home.html",context)
 
 def products_gpu_view(request,*args,**kwargs):
+    products=Product.objects.all().filter(category="GPU")
+    sortvalue=0
+    if request.method == 'POST':
+        case=int(request.POST.get('sort'))
+        if(case == 1):
+            sortvalue=1
+            products = Product.objects.all().filter(category="GPU").order_by("min_price")
+        elif(case == 2):
+            sortvalue=2
+            products = Product.objects.all().filter(category="GPU").order_by("-min_price")
+        elif(case == 3):
+            sortvalue=3
+            products = Product.objects.all().filter(category="GPU").order_by("title")
+        elif(case == 4):
+            sortvalue=4
+            products = Product.objects.all().filter(category="GPU").order_by("-title")
     context = {
-    'all_products':Product.objects.filter(category="GPU").all()
+        'sortvalue':sortvalue,
+        'all_products': products
     }
     return render(request,"home.html",context)
 def products_ram_view(request,*args,**kwargs):
+    products=Product.objects.all().filter(category="RAM")
+    sortvalue=0
+    if request.method == 'POST':
+        case=int(request.POST.get('sort'))
+        if(case == 1):
+            sortvalue=1
+            products = Product.objects.all().filter(category="RAM").order_by("min_price")
+        elif(case == 2):
+            sortvalue=2
+            products = Product.objects.all().filter(category="RAM").order_by("-min_price")
+        elif(case == 3):
+            sortvalue=3
+            products = Product.objects.all().filter(category="RAM").order_by("title")
+        elif(case == 4):
+            sortvalue=4
+            products = Product.objects.all().filter(category="RAM").order_by("-title")
     context = {
-    'all_products':Product.objects.filter(category="RAM").all()
+        'sortvalue':sortvalue,
+        'all_products': products
     }
     return render(request,"home.html",context)
 def products_cpu_view(request,*args,**kwargs):
+    products=Product.objects.all().filter(category="CPU")
+    sortvalue=0
+    if request.method == 'POST':
+        case=int(request.POST.get('sort'))
+        if(case == 1):
+            sortvalue=1
+            products = Product.objects.all().filter(category="CPU").order_by("min_price")
+        elif(case == 2):
+            sortvalue=2
+            products = Product.objects.all().filter(category="CPU").order_by("-min_price")
+        elif(case == 3):
+            sortvalue=3
+            products = Product.objects.all().filter(category="CPU").order_by("title")
+        elif(case == 4):
+            sortvalue=4
+            products = Product.objects.all().filter(category="CPU").order_by("-title")
     context = {
-    'all_products':Product.objects.filter(category="CPU").all()
+        'sortvalue':sortvalue,
+        'all_products': products
     }
     return render(request,"home.html",context)
 
 def products_mobo_view(request,*args,**kwargs):
+    products=Product.objects.all().filter(category="MOBO")
+    sortvalue=0
+    if request.method == 'POST':
+        case=int(request.POST.get('sort'))
+        if(case == 1):
+            sortvalue=1
+            products = Product.objects.all().filter(category="MOBO").order_by("min_price")
+        elif(case == 2):
+            sortvalue=2
+            products = Product.objects.all().filter(category="MOBO").order_by("-min_price")
+        elif(case == 3):
+            sortvalue=3
+            products = Product.objects.all().filter(category="MOBO").order_by("title")
+        elif(case == 4):
+            sortvalue=4
+            products = Product.objects.all().filter(category="MOBO").order_by("-title")
     context = {
-    'all_products':Product.objects.filter(category="MOBO").all()
+        'sortvalue':sortvalue,
+        'all_products': products
     }
     return render(request,"home.html",context)
